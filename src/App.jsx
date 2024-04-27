0b34712414744e313cc9cfaee0298c7d02883c53
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-04-27 19:24:33 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import config from './Config/config';
+
+function App() {
+    console.log(config.appwriteUrl);
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
